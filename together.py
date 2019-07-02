d2f4606f04bec691b0759da77fba8dbc140a11d3
김반석 <2015004393@hanyang.ac.kr>
2019-07-02 23:12:52 +0900

    explain  variables that are  filepath, number and array

--- together.py
+++ together.py
@@ -5,13 +5,13 @@ array = []
 def opentxt():
     global number, array
     count = 0
-    filepath = sys.path[0] + "\\1.txt"
+    filepath = sys.path[0] + "/1.txt" #window일때는 "\\1.txt" 다른 os 일떄는 "/1.txt"
     lines=open(filepath,'r').readlines()
     for line in lines:
         if len(line)==2:
-            number.append(line.split("\n")[0])
+            number.append(line.split("\n")[0]) #number에 ['5','6']형태로 들어감
         elif len(line)>2:
-            array.insert(count,line.split("\n")[0])
+            array.insert(count,line.split("\n")[0]) #array에 ['[9,20,...]','[30,1,...],...]형태로 들어감
             count += 1
 
 def main():
