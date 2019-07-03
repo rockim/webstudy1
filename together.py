a15ab32448f220d53ebf64752882bf45f9d2e49b
rockim <rlaqkstjr96@gmail.com>
2019-07-03 14:25:04 +0900

    explain code(filepath,number and array)

--- together.py
+++ together.py
@@ -5,13 +5,13 @@ array = []
 def opentxt():
     global number, array
     count = 0
-    filepath = sys.path[0] + "\\1.txt"
+    filepath = sys.path[0] + "\\1.txt" #window일떄 "\\1.txt" linux 나 os X 일떄 "/1.txt"
     lines=open(filepath,'r').readlines()
     for line in lines:
         if len(line)==2:
-            number.append(line.split("\n")[0])
+            number.append(line.split("\n")[0]) #number < ["5","6"]
         elif len(line)>2:
-            array.insert(count,line.split("\n")[0])
+            array.insert(count,line.split("\n")[0]) #array < ["[9,20,...]","[...]",...]
             count += 1
 
 def main():
