07d3c47da777083e4d44a42ee8bcac295e6c40d1
rockim <rlaqkstjr96@gmail.com>
2019-07-04 16:07:44 +0900

    read input file and store at ar1 and ar2

--- together.py
+++ together.py
@@ -1,6 +1,42 @@
 import sys
-number = []
+import numpy as np
+number = 0
 array = []
+def make_Array():
+    arr1 = np.identity(number)
+    arr2 = np.identity(number)
+
+    for i in range(number):
+        for j in range(number):
+            arr1[i][j]
+  #  for i in range(int(number[acount])):
+        
+def convert_array():
+    ar1 = []
+    ar2 = []
+    count=0
+    print(array)
+    for i in array:
+        if i == array[0]:
+            ar1.insert(count,i.split(", ")[0].split("[")[1])
+            count += 1
+            for j in range(number-1):
+                if j != 0:
+                    ar1.insert(count,i.split(", ")[j])
+                    count += 1
+            ar1.insert(count,i.split(", ")[number-1].split("]")[0])
+            count += 1
+        else:
+            ar2.insert(count,i.split(", ")[0].split("[")[1])
+            count += 1
+            for j in range(number-1):
+                if j != 0:
+                    ar2.insert(count,i.split(", ")[j])
+                    count += 1
+            ar2.insert(count,i.split(", ")[number-1].split("]")[0])
+            count += 1
+    print(ar1, ar2) # array 에 받은 것을 ar1 과 ar2 로 나누어서 저장
+#def convert_binary():
 
 def opentxt():
     global number, array
@@ -9,13 +45,18 @@ def opentxt():
     lines=open(filepath,'r').readlines()
     for line in lines:
         if len(line)==2:
-            number.append(line.split("\n")[0]) #number < ["5","6"]
+            number= int(line.split("\n")[0]) #number = 5
         elif len(line)>2:
             array.insert(count,line.split("\n")[0]) #array < ["[9,20,...]","[...]",...]
             count += 1
-
+    #print(array[0].split(", ")[0].split("[")[1])
+    #print(array[0].split(", ")[number-1].split("]")[0])
+    convert_array()
 def main():
     opentxt()
+    #for num in range(len(number)):
+     #   number[num]
 
+        
 if __name__ == "__main__":
     main()
